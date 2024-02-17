c4db9985ed60620fb08e08f45a090bcfbc1bf7da
Aaron Otaguro <aaron.otaguro@gmail.com>
2024-02-17 10:38:08 -0800

    test

---
+++ script.js
@@ -0,0 +1,14 @@
+// Initialize score
+var score = 0;
+var beatcost = 15;
+var beats = 0;
+// Update score display
+// Click event listener
+function addToScore(amount) {
+   score = score + amount;
+   document.getElementById("score").innerHTML = score;
+};
+
+
+
+
