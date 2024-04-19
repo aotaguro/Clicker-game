4f10d7e082ec6e8e72b0febbd09fa7b890670ced
Aaron Otaguro <aaron.otaguro@gmail.com>
2024-04-18 17:29:49 -0700

    stuff

--- script.js
+++ script.js
@@ -2,47 +2,58 @@
 var score = 0;
 var beatcost = 15;
 var beats = 0;
-var melodyCost = 100; 
-var melodies = 0; 
-var clickingPower = 1; 
+var melodyCost = 100;
+var melodies = 0;
+var clickingPower = 1;
 
 
 
 
-function buybeat() { 
-  if(score >= beatcost) { 
-    score = score - beatcost; 
-    beats = beats + 1; 
-    beatcost = Math.round(beatcost * 1.15);
-    
-    document.getElementById("score").innerHTML = score;
-    document.getElementById("beatcost").innerHTML = beatcost;
-    document.getElementById("beats").innerHTML = beats;
-  }
+function buybeat() {
+    if (score >= beatcost) {
+        score = score - beatcost;
+        beats = beats + 1;
+        beatcost = Math.round(beatcost * 1.15);
+
+        document.getElementById("score").innerHTML = score;
+        document.getElementById("beatcost").innerHTML = beatcost;
+        document.getElementById("beats").innerHTML = beats;
+        updateScorePerSecond();
+    }
 }
 
 
-function buyMelody() { 
-    if(score >= melodyCost) { 
-      score = score - melodyCost; 
-      melodies = melodies + 1; 
-      melodyCost = Math.round(melodyCost * 1.15);
-      
-      document.getElementById("score").innerHTML = score;
-      document.getElementById("melodyCost").innerHTML = melodyCost;
-      document.getElementById("melodies").innerHTML = melodies;
+function buyMelody() {
+    if (score >= melodyCost) {
+        score = score - melodyCost;
+        melodies = melodies + 1;
+        melodyCost = Math.round(melodyCost * 1.15);
+
+        document.getElementById("score").innerHTML = score;
+        document.getElementById("melodyCost").innerHTML = melodyCost;
+        document.getElementById("melodies").innerHTML = melodies;
+        updateScorePerSecond();
     }
-  }
+}
 
 function addToScore(amount) {
-   score = score + amount;
-   document.getElementById("score").innerHTML = score;
+    score = score + amount;
+    document.getElementById("score").innerHTML = score;
 };
 
-setInterval (function() { 
-    score = score + beats; 
-    score = score + melodies * 5; 
+
+function updateScorePerSecond() {
+    vibespersecond = beats + melodies * 5;
+    document.getElementById("vibespersecond").innerHTML = vibespersecond;
+}
+
+
+setInterval(function () {
+    score = score + beats;
+    score = score + melodies * 5;
     document.getElementById("score").innerHTML = score;
+
+    document.title = score + " Vibes - Vibe Clicker";
 }, 1000); //1000ms = 1 sec 
 
 
